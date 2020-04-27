e29bd87eb7fcdb19dc85248b169118253eac74a5
Pablo Garzon <pa.garzon@uniandes.edu.co>
2020-04-27 18:16:38 -0500

    rutas-conductor completo

--- src/app/conductor/conductor.module.ts
+++ src/app/conductor/conductor.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListarConductorComponent } from './listar-Conductor/listar-Conductor.component';
 import { ConductorDetailComponent } from './conductor-detail/conductor-detail.component';
+import { conductorRoutingModule } from './conductor-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, conductorRoutingModule
   ],
   declarations: [ListarConductorComponent, ConductorDetailComponent],
   exports:[ListarConductorComponent]
